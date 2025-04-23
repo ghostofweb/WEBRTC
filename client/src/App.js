ee95b830d426372fed5393664bdb49c1944329a6
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-04-23 16:21:27 +0530

    Refactor code structure: clean up imports and enhance socket handling in server and client styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes,Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 function App() {
   return (
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import Server from "socket.io"
+import {Server} from "socket.io"
 
 
 const io = new Server();
@@ -8,10 +8,21 @@ const port = 8000
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+const emailToSocketMapping = new Map()
+
 io.on("connection",socket=>{
-    
+    socket.on("join-room",data =>{
+        const {roomId,emailId} = data
+        console.log("user",emailId,"joined room",roomId);
+        if(!emailToSocketMapping.has(emailId)){
+            emailToSocketMapping.set(emailId,socket.id)
+        socket.join(roomId)
+        socket.broadcast.to(roomId).emit("user-joined",{emailId})
+    })
 })
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
 })
+
 io.listen(8001);
